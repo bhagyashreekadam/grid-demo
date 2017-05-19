6202702bd3167caff8aa6d5b89b6ac7fa4740655
bhagyashreekadamsynerzip <bhagyashree.kadam@synerzip.com>
2017-05-19 12:51:23 +0530

    first commit

---
+++ V1__create_customer_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE employee (id int(6) NOT NULL,firstname varchar(50) NOT NULL,lastname varchar(50) NOT NULL,age int(10) NOT NULL,PRIMARY KEY (id));
---
+++ src/DemoCLass.groovy
@@ -0,0 +1,8 @@
+
+class DemoCLass {
+
+	static main(args) {
+	println'HIiiiiiiiiiii bhagu';
+	}
+
+}
